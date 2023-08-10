b591f8f08146360b11f021c744cddd8b7f2b6438
cregs08 <cole.regnier@gmail.com>
2023-08-10 20:15:42 -0300

    fixing imports

--- Payload/payload.py
+++ Payload/payload.py
@@ -42,21 +42,3 @@ class Payload:
     def send(self):
         print(self.get_formatted_detections())
 
-# Mocking the detections and usage:
-class Detection:
-    def __init__(self, confidence, class_id):
-        self.confidence = confidence
-        self.class_id = class_id
-
-
-# For this example, consider 0 represents "person" and 1 represents "phone".
-detections = [Detection(0.95, 0), Detection(0.87, 1), Detection(0.78, 0), Detection(0.60, 1)]
-target_classes = ["person", "phone"]
-conf = 0.80
-
-# Usage
-payload = Payload(detections, target_classes, conf)
-formatted_detections = payload.get_formatted_detections()
-
-
-payload.send()
