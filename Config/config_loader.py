0953968d24e8064b002cd203536dd58721e5e703
cregs08 <cole.regnier@gmail.com>
2023-09-03 18:56:28 +0800

    updating names and adding in load label maps

--- Config/config_loader.py
+++ Config/config_loader.py
@@ -3,14 +3,15 @@ import os
 
 
 class ConfigLoader:
-    def __init__(self, project_dir):
+    def __init__(self, config_dir):
         """
         loads in config if no file path is spcified it loads it the path stored in config path. Must run update config
         for it to be accurate
         :param file_path:
         """
-        self.project_dir = project_dir
-        self.config_path = os.path.join(project_dir, 'config.json')
+        self.config_dir = config_dir
+        self.project_dir = os.path.dirname(config_dir)
+        self.config_path = os.path.join(config_dir, 'config.json')
         self.config = self.load_config()
 
     def load_config(self):
@@ -30,20 +31,40 @@ class ConfigLoader:
             # Add your other default configurations here...
         }
 
-    def write_label_map(self, project_dir, class_lists=[]):
+    def load_label_maps(self):
+        label_map_dir = os.path.join(self.project_dir, 'LabelMaps')
+        # Initialize an empty dictionary to hold the label maps
+        label_maps = {}
+
+        # Loop through each file in the LabelMaps directory
+        for filename in os.listdir(label_map_dir):
+            if filename.endswith(".txt"):
+                filepath = os.path.join(label_map_dir, filename)
+
+                # Read the content of the txt file into a list
+                with open(filepath, 'r') as f:
+                    lines = f.readlines()
+
+                # Remove newline characters and store in dictionary
+                label_maps[filename[:-4]] = [line.strip() for line in lines]
+
+        # Add label maps to config
+        self.config['label_maps'] = label_maps
+
+    def update_config(self):
+
+        with open(self.config_path, 'w') as file:
+            json.dump(self.config, file, indent=4)
+
+    def write_label_map(self, config_dir, class_lists=[]):
             for classes in class_lists:
-                label_map_path = os.path.join(project_dir, f'label_map_{classes}.txt')
+                label_map_path = os.path.join(config_dir, f'label_map_{classes}.txt')
                 self.config['label_maps'][classes] = label_map_path
                 labels = self.config['class_lists'][classes]
                 with open(label_map_path, 'w') as file:
                     for label in labels:
                         file.write(str(label) + '\n')
                 self.update_config()
-
-    def update_config(self):
-
-        with open(self.config_path, 'w') as file:
-            json.dump(self.config, file, indent=4)
 
     def update_paths(self):
 
@@ -66,4 +87,5 @@ class ConfigLoader:
         with open(self.config_path, 'w') as file:
             json.dump(config, file, indent=4)
         print("Config file updated successfully.")
+
 
