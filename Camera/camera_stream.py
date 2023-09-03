1b945d44d6d07bd07c8c61726bb21959dfce85c6
cregs08 <cole.regnier@gmail.com>
2023-09-03 21:10:18 +0800

    first

--- Camera/camera_stream.py
+++ Camera/camera_stream.py
@@ -1,10 +1,12 @@
 import cv2
+import time
 
 
 class CameraStream:
     def __init__(self, camera_index=0):
         """Initializes the webcam stream."""
         self.cap = cv2.VideoCapture(camera_index)
+        self.last_time = None
 
         if not self.cap.isOpened():
             print("Could not open webcam!")
@@ -19,11 +21,24 @@ class CameraStream:
             print("Could not read frame!")
             exit()
 
+    def show_fps(self, frame):
+        """Calculates and displays FPS on the frame."""
+        if self.last_time is None:
+            self.last_time = time.time()
+            fps = 0
+        else:
+            curr_time = time.time()
+            fps = 1 / (curr_time - self.last_time)
+            self.last_time = curr_time
+
+        # Put FPS on the frame
+        cv2.putText(frame, f"FPS: {fps:.2f}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+
     def show_stream(self):
-        """Displays the webcam stream."""
         while True:
             ret, self.frame = self.cap.read()
             if ret:
+                self.show_fps(self.frame)  # Display FPS
                 cv2.imshow('Camera Stream', self.frame)
 
             if cv2.waitKey(1) & 0xFF == ord('q'):
