cdc442bb8e5f96f2e72cb7c0569ba2e08367c626
cregs08 <cole.regnier@gmail.com>
2023-09-03 21:35:53 +0800

    first

--- efficient_net_model.py
+++ efficient_net_model.py
@@ -1,26 +1,13 @@
-import cv2
-import numpy as np
-from ModelInstances.tf_lite_base import TFLite
-from supervision import Detections
-from typing import List, Tuple
+from Config.default_config import default_config
+from ModelInstances.efficient_net.efficient_net_model import EfficientNetTFLite
+from ModelInstances.efficient_net.prediction_processor_enet import PredictionProcessorENet
+from Camera.camera_detect import CameraStreamDetect
 
+model_path = default_config['model_paths']['efficient-net']
 
-class EfficientNetTFLite(TFLite):
+enet = EfficientNetTFLite(model_path=model_path,
+                                  labels=default_config['label_maps']['imagenet'],
+                                  prediction_processor=PredictionProcessorENet)
 
-    def preprocess_image(self, image):
-        input_shape = self.input_details[0]['shape'][1:3]  # Shape as [Height, Width]
-        image_resized = cv2.resize(image, tuple(input_shape[::-1]))  # OpenCV uses (W, H) so reverse shape to (H, W)
-        input_data = np.expand_dims(image_resized, axis=0).astype(np.uint8)  # Convert to UINT8
-
-        return input_data
-
-    def detect_and_process(self, image) -> Tuple[Detections, List[str]]:
-        outputs = self.detect(image)
-        preds = self.prediction_processor(outputs)
-        detections = preds.process_predictions(outputs)
-
-        labels = []
-        if detections:
-            labels = [self.id_label_map[class_id] for class_id in detections.class_id]
-        print(labels)
-        return detections, labels
+camera = CameraStreamDetect(model=enet)
+camera.show_stream()
